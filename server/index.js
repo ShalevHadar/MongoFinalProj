eea77b56508d18b09baad06dc8288a0d1cb3aaa3
Shalev Hadar <shdar94@gmail.com>
2022-05-19 23:33:46 +0300

    react app & server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+// import all required dependencies for the software to run
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+
+// initialize environment configuration
+dotenv.config();
+const port = process.env.PORT;
+const dbUrl = process.env.DB_PASS;
+
+// setup http middleware
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// create connection to db
+mongoose.connect(dbUrl, (err) => {
+  if (err) {
+    console.log("Connection to DB fail");
+    console.error(err);
+  } else {
+    console.log("Connection to DB succefful");
+    app.listen(port, () => {
+      console.log(`App is running on port: ${port}`);
+    });
+  }
+});
