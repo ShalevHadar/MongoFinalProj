2f62fad83f5bb727d6cc76c54ce9342f8fe3b7b2
Shalev Hadar <shdar94@gmail.com>
2022-05-20 11:58:52 +0300

    Front-redo

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios';
 
 function App() {
+
+  const [id, setId] = useState('');
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+  }
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    //setId({value: event.target.value});
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={id} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
--- server/costs/cost-helper.js
+++ server/costs/cost-helper.js
@@ -9,30 +9,35 @@ const createItem = async (data) => {
     category: data.category,
   });
 
-  await item.save();
-
   const user = await UserModel.updateOne(
     { personal_id: data.personal_id },
     {$push: { items: item._id }, $inc: {sum: item.price}}
   );
 
   if (!user) {
-    throw new Error("no such user;");
+    throw new Error("no such user");
   }
 
+  await item.save();
   
 };
 
 const deleteItem = async (data) => {
-  await UserModel.updateOne(
-    { personal_id: data.personal_id },
-    { $pull: { items: data.id } }
-  );
 
-  await costModel.deleteOne({
+  const item = await costModel.findOneAndDelete({
     _id: data.id,
   });
 
+  if (!item) {
+    throw new Error("no such item");
+  }
+
+  await UserModel.updateOne(
+    { personal_id: data.personal_id },
+    { $pull: { items: data.id }, $inc: {sum: item.price*-1} }
+  );
+
+  
 };
 
 module.exports = {
